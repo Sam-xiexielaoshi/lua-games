1bbf7538d00106eb958db5dbb02a9670fc1e5db2
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-07-05 13:42:03 +0530

    conf file issue

---
+++ Save_The_Ball/conf.lua
@@ -0,0 +1,12 @@
+function love.conf(t)
+    t.window.title = 'Save the Ball'
+    t.window.width = 800
+    t.window.height = 600
+    t.window.resizable = true
+    t.window.minwidth = 400
+    t.window.minheight = 300
+    t.window.fullscreen = false
+    t.window.vsync = 1
+    t.window.msaa = 4
+    t.window.display = 2
+end
