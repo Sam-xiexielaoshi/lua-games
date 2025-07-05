ed17ef700fcb9c779424d49282d7c4538eea3819
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-07-05 21:55:10 +0530

    basic setup for the game

--- Save_The_Ball/main.lua
+++ Save_The_Ball/main.lua
@@ -1,13 +1,37 @@
 local love = require('love')
 
+local game = {
+    state = {
+        menu = true,
+        paused = false,
+        running = false,
+        ended = false,
+    }
+}
+
+local player = {
+    radius = 20,
+    x=30,
+    y=30
+}
+
 function love.load()
-    
+    love.mouse.setVisible(false)
 end
 
 function love.update(dt)
-    
+    player.x, player.y = love.mouse.getPosition()
+
 end
 
 function love.draw()
-    
+    love.graphics.printf("FPS: "..love.timer.getFPS(), love.graphics.newFont(16),10,love.graphics.getHeight()-30,love.graphics.getWidth())
+
+    if game.state['running'] then
+        love.graphics.circle('fill', player.x,player.y, player.radius/2)
+    end
+
+    if not game.state['running'] then
+        love.graphics.arc('fill', player.x,player.y,player.radius,0, math.pi / 2,500)
+    end
 end
