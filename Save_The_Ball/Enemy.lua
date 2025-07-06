0ff2a04877bd84362343ae0fdef38f471cdc54d3
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-07-06 16:50:39 +0530

    creating the enemy and collision objects including the spawn randomness

---
+++ Save_The_Ball/Enemy.lua
@@ -0,0 +1,71 @@
+local love = require 'love'
+
+function Enemy()
+    local dice, _x, _y = math.random(1,4)
+    local _radius = 10
+
+    -- Use the commented version if you want things to spawn randomly within or near the screen area, like power-ups or particles.
+
+    -- if dice == 1 then
+    --     _x = math.random(_radius, love.graphics.getWidth()-_radius)
+    --     _y = math.random(_radius, love.graphics.getHeight()-_radius)  -- Fixed: changed getWidth() to getHeight()
+    
+    -- elseif dice == 2 then
+    --     _x = math.random(_radius, love.graphics.getWidth()-_radius)
+    --     _y = math.random(love.graphics.getHeight()-_radius, love.graphics.getHeight()+_radius)
+    -- elseif dice == 3 then
+    --     _x = math.random(love.graphics.getWidth()-_radius, love.graphics.getWidth())
+    --     _y = math.random(_radius, love.graphics.getHeight()-_radius)
+    -- elseif dice == 4 then
+    --     _x = math.random(love.graphics.getWidth()-_radius, love.graphics.getWidth())
+    --     _y = math.random(_radius, love.graphics.getHeight()-_radius)
+    -- else
+    --     _x = 100
+    --     _y = 100
+    -- end
+
+    -- Use the active version when you want objects (e.g., enemies, projectiles) to fly in from outside the screen
+    if dice == 1 then
+        _x = math.random(_radius, love.graphics.getWidth())
+        _y = -_radius * 4
+    elseif dice == 2 then
+        _x = -_radius * 4
+        _y = math.random(_radius, love.graphics.getHeight())
+    elseif dice == 3 then
+        _x = math.random(_radius, love.graphics.getWidth())
+        _y = love.graphics.getHeight()+(_radius *4)
+    else 
+        _x = love.graphics.getWidth()+(_radius *4)
+        _y = math.random(_radius, love.graphics.getHeight())
+    end
+
+
+    return {
+        level =1,
+        radius = _radius,
+        x = _x,
+        y=_y,
+
+        move = function(self, player_x, player_y)
+            if player_x - self.x >0 then
+                self.x = self.x + self.level
+            elseif  player_x - self.x <0 then
+                self.x = self.x - self.level
+            end
+
+            if player_y - self.y>0 then
+                self.y = self.y + self.level
+            elseif player_y - self.y <0 then
+                self.y = self.y - self.level
+            end
+        end,
+        
+        draw = function(self)
+            love.graphics.setColor(1,0.5,0.7)
+            love.graphics.circle('fill', self.x, self.y, self.radius)
+            love.graphics.setColor(1,1,1)
+        end
+    }
+end
+
+return Enemy
--- Save_The_Ball/main.lua
+++ Save_The_Ball/main.lua
@@ -1,10 +1,16 @@
 local love = require 'love'
+local Enemy = require 'Enemy'
+
+math.randomseed(os.time())
 
 local game = {
+
+    difficulty = 1,
+
     state = {
-        menu = true,
+        menu = false,
         paused = false,
-        running = false,
+        running = true,
         ended = false,
     }
 }
@@ -15,19 +21,28 @@ local player = {
     y=30
 }
 
+local enemies = {}
+
 function love.load()
     love.mouse.setVisible(false)
+    table.insert(enemies,1,Enemy())
 end
 
 function love.update(dt)
     player.x, player.y = love.mouse.getPosition()
-
+    for i=1, #enemies do
+        enemies[i]:move(player.x, player.y)
+    end
 end
 
 function love.draw()
     love.graphics.printf("FPS: "..love.timer.getFPS(), love.graphics.newFont(16),10,love.graphics.getHeight()-30,love.graphics.getWidth())
 
     if game.state['running'] then
+        for i=1, #enemies do
+        enemies[i]:draw()
+    end
+
         love.graphics.circle('fill', player.x,player.y, player.radius/2)
     end
 
