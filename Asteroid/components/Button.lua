8ae14ad72b427aa35637fa6cb16cc372a43bcd52
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-08-09 13:38:03 +0530

    added new package lunajson for the new highscore implementation not completed yet

--- Asteroid/components/Button.lua
+++ Asteroid/components/Button.lua
@@ -1,8 +1,9 @@
 local love = require "love"
 
-local Text = require "components/Text"
+local Text = require "components.Text"
 
-function Button(func, text_color, button_color, width, height, text, text_align, font_size, button_x, button_y, text_x, text_y)
+function Button(func, text_color, button_color, width, height, text, text_align, font_size, button_x, button_y, text_x,
+                text_y)
     local btn_text = {}
     func = func or function() print("This button has no function attached") end
 
@@ -18,7 +19,7 @@ function Button(func, text_color, button_color, width, height, text, text_align,
         btn_text.x = button_x
     end
     return {
-        text_color = text_color or { r = 1, g = 1, b = 1 }, -- white
+        text_color = text_color or { r = 1, g = 1, b = 1 },     -- white
         button_color = button_color or { r = 0, g = 0, b = 0 }, -- black
         width = width or 100,
         height = height or 100,
@@ -28,7 +29,7 @@ function Button(func, text_color, button_color, width, height, text, text_align,
         button_x = button_x or 0,
         button_y = button_y or 0,
         text_component = Text(
-            text, 
+            text,
             btn_text.x,
             btn_text.y,
             font_size,
@@ -39,15 +40,15 @@ function Button(func, text_color, button_color, width, height, text, text_align,
             1
         ),
 
-        setButtonColor = function (self, red, green, blue)
-            self.button_color = { r = red, g = green, b = blue}
+        setButtonColor = function(self, red, green, blue)
+            self.button_color = { r = red, g = green, b = blue }
         end,
 
-        setTextColor = function (self, red, green, blue)
-            self.text_color = { r = red, g = green, b = blue}
+        setTextColor = function(self, red, green, blue)
+            self.text_color = { r = red, g = green, b = blue }
         end,
 
-        checkHover = function (self, mouse_x, mouse_y, cursor_radius)
+        checkHover = function(self, mouse_x, mouse_y, cursor_radius)
             if (mouse_x + cursor_radius >= self.button_x) and (mouse_x - cursor_radius <= self.button_x + self.width) then
                 if (mouse_y + cursor_radius >= self.button_y) and (mouse_y - cursor_radius <= self.button_y + self.height) then
                     return true
@@ -57,25 +58,25 @@ function Button(func, text_color, button_color, width, height, text, text_align,
             return false
         end,
 
-        click = function (self)
+        click = function(self)
             func()
         end,
 
-        draw = function (self)
+        draw = function(self)
             love.graphics.setColor(self.button_color["r"], self.button_color["g"], self.button_color["b"])
             love.graphics.rectangle("fill", self.button_x, self.button_y, self.width, self.height)
-        
+
             self.text_component:setColor(self.text_color["r"], self.text_color["g"], self.text_color["b"])
             self.text_component:draw()
-        
+
             love.graphics.setColor(1, 1, 1)
         end,
 
-        getPos = function (self)
+        getPos = function(self)
             return self.button_x, self.button_y
         end,
 
-        getTextPos = function (self)
+        getTextPos = function(self)
             return self.text_x, self.text_y
         end
     }
--- Asteroid/states/Menu.lua
+++ Asteroid/states/Menu.lua
@@ -4,23 +4,26 @@ local Button = require "components/Button"
 
 function Menu(game, player)
     local funcs = {
-        newGame = function ()
+        newGame = function()
             game:startNewGame(player)
         end,
-        quitGame = function ()
+        quitGame = function()
             love.event.quit()
         end,
     }
 
     local buttons = {
-        Button(funcs.newGame, nil, nil, love.graphics.getWidth() / 3, 50, "New Game", "center", "h3", love.graphics.getWidth() / 3, love.graphics.getHeight() * 0.25),
-        Button(nil, nil, nil, love.graphics.getWidth() / 3, 50, "Settings", "center", "h3", love.graphics.getWidth() / 3, love.graphics.getHeight() * 0.4),
-        Button(funcs.quitGame, nil, nil, love.graphics.getWidth() / 3, 50, "Quit", "center", "h3", love.graphics.getWidth() / 3, love.graphics.getHeight() * 0.55),
+        Button(funcs.newGame, nil, nil, love.graphics.getWidth() / 3, 50, "New Game", "center", "h3",
+            love.graphics.getWidth() / 3, love.graphics.getHeight() * 0.25),
+        Button(nil, nil, nil, love.graphics.getWidth() / 3, 50, "Settings", "center", "h3", love.graphics.getWidth() / 3,
+            love.graphics.getHeight() * 0.4),
+        Button(funcs.quitGame, nil, nil, love.graphics.getWidth() / 3, 50, "Quit", "center", "h3",
+            love.graphics.getWidth() / 3, love.graphics.getHeight() * 0.55),
     }
 
     return {
         focused = "",
-        run = function (self, clicked)
+        run = function(self, clicked)
             local mouse_x, mouse_y = love.mouse.getPosition()
             for name, button in pairs(buttons) do
                 if button:checkHover(mouse_x, mouse_y, 10) then
@@ -37,7 +40,7 @@ function Menu(game, player)
             end
         end,
 
-        draw = function (self)
+        draw = function(self)
             for _, button in pairs(buttons) do
                 button:draw()
             end
